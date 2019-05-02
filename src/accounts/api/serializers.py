d80a5babf19500d2b3cd88c59a839c4f1c39aefe
omar jandali <omarjandali@omars-mbp.home>
2019-05-01 19:01:34 -0700

    got rid of the pycache to fix merge issues when merging with master

---
+++ src/accounts/api/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from ..models import Account, Transaction
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = (
+            'user',
+            'name',
+            'bank',
+            'type',
+            'amount',
+            'primary',
+            'acct_ids'
+        )
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = (
+            'user_from',
+            'user_to',
+            'acct_from',
+            'acct_to',
+            'amount',
+            'description'
+        )
