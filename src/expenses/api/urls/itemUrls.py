c6745d25e4597f30e13ea9794fe4712cfb4f37f9
omar jandali <omarjandali@omars-mbp.lan>
2019-05-11 15:29:47 -0700

    I added the viewsets and routers for the expenses models

--- src/expenses/api/urls/itemUrls.py
+++ src/expenses/api/urls/itemUrls.py
@@ -1,15 +1,22 @@
-from django.urls import path
+from expenses.api.views.ItemView import ItemViewSet
+from rest_framework.routers import DefaultRouter
 
-from expenses.api.views.itemView import ItemCreateView, ItemListView, ItemRetrieveView
-from expenses.api.views.itemView import ItemDeleteView, ItemUpdateView
+router = DefaultRouter()
+router.register(r'', ItemViewSet, base_name='item')
+urlpatterns = router.urls
 
-urlpatterns = [
-    path('', ItemListView.as_view()),
-    path('create/', ItemCreateView.as_view()),
-    path('update/<pk>/', ItemUpdateView.as_view()),
-    path('delete/<pk>/', ItemDeleteView.as_view()),
-    path('<pk>/', ItemRetrieveView.as_view())
-]
+# from django.urls import path
+#
+# from expenses.api.views.itemView import ItemCreateView, ItemListView, ItemRetrieveView
+# from expenses.api.views.itemView import ItemDeleteView, ItemUpdateView
+#
+# urlpatterns = [
+#     path('', ItemListView.as_view()),
+#     path('create/', ItemCreateView.as_view()),
+#     path('update/<pk>/', ItemUpdateView.as_view()),
+#     path('delete/<pk>/', ItemDeleteView.as_view()),
+#     path('<pk>/', ItemRetrieveView.as_view())
+# ]
 
 # api/group/member/
 # api/group/member/create
--- src/expenses/api/views/expenseView.py
+++ src/expenses/api/views/expenseView.py
@@ -1,26 +1,34 @@
-from expenses.models import Expense
+from groups.models import Expense
+from ..serializers import ExpensesSerializers
+from rest_framework import viewsets
 
-from expenses.api.serializers import ExpensesSerializers
-
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
-from rest_framework.generics import UpdateAPIView, DestroyAPIView
-
-class ExpenseCreateView(CreateAPIView):
+class ExpenseViewSet(viewsets.ModelViewSet):
     queryset = Expense.objects.all()
     serializer_class = ExpensesSerializers
 
-class ExpenseListView(ListAPIView):
-    queryset = Expense.objects.all()
-    serializer_class = ExpensesSerializers
-
-class ExpenseRetrieveView(RetrieveAPIView):
-    queryset = Expense.objects.all()
-    serializer_class = ExpensesSerializers
-
-class ExpenseUpdateView(UpdateAPIView):
-    queryset = Expense.objects.all()
-    serializer_class = ExpensesSerializers
-
-class ExpenseDeleteView(DestroyAPIView):
-    queryset = Expense.objects.all()
-    serializer_class = ExpensesSerializers
+# from expenses.models import Expense
+#
+# from expenses.api.serializers import ExpensesSerializers
+#
+# from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
+# from rest_framework.generics import UpdateAPIView, DestroyAPIView
+#
+# class ExpenseCreateView(CreateAPIView):
+#     queryset = Expense.objects.all()
+#     serializer_class = ExpensesSerializers
+#
+# class ExpenseListView(ListAPIView):
+#     queryset = Expense.objects.all()
+#     serializer_class = ExpensesSerializers
+#
+# class ExpenseRetrieveView(RetrieveAPIView):
+#     queryset = Expense.objects.all()
+#     serializer_class = ExpensesSerializers
+#
+# class ExpenseUpdateView(UpdateAPIView):
+#     queryset = Expense.objects.all()
+#     serializer_class = ExpensesSerializers
+#
+# class ExpenseDeleteView(DestroyAPIView):
+#     queryset = Expense.objects.all()
+#     serializer_class = ExpensesSerializers
